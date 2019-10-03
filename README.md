67ecb4f6e64d178f48fa2ed24b9f2b9247d8b2dd
Guillermo Varela <guillermo.vchoucino@udc.es>
2019-10-03 19:41:04 +0200

    inicializado proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto inicializado con https://github.com/jonashackt/spring-boot-vuejs
---
+++ backend/src/main/java/fic/riws/eli5riws/Eli5RiwsApplication.java
@@ -0,0 +1,13 @@
+package fic.riws.eli5riws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eli5RiwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eli5RiwsApplication.class, args);
+	}
+
+}
