b730b2294483f35c30810a27206d496a5513612f
Luis Cruz <luis.cruz@udc.es>
2019-10-24 19:05:19 +0200

    Inyección estática de servicio de preguntas para poblar la bd mediante el crawler

--- backend/src/main/java/fic/riws/eli5riws/Eli5RiwsApplication.java
+++ backend/src/main/java/fic/riws/eli5riws/Eli5RiwsApplication.java
@@ -1,13 +1,23 @@
 package fic.riws.eli5riws;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import fic.riws.eli5riws.service.QuestionService;
 
 @SpringBootApplication
 public class Eli5RiwsApplication {
 
+	@Autowired
+    private static QuestionService questionService;
+
+    private static ApplicationContext context;
+
 	public static void main(String[] args) {
-		SpringApplication.run(Eli5RiwsApplication.class, args);
+		context = SpringApplication.run(Eli5RiwsApplication.class, args);
+        questionService = (QuestionService) context.getBean("questionService");
 	}
 
 }
