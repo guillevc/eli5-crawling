194d4a4c9a7a1e2a345782101199d4009bc251f6
Guillermo Alfonso Varela Chouciño <guillermo.vchoucino@udc.es>
2019-11-21 18:58:32 +0100

    karma en el front y correcciones

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
       <div class="content has-text-centered">
         <p>
-          <strong>Crawling and searching <a href="https://reddit.com/r/explainlikeimfive" target="_blank">reddit.com/r/explainlikeimfive</a></strong> by <a href="https://github.com/guillevc" target="_blank">Guillermo Varela</a> and <a href="#" target="_blank">Luis Cruz</a>.
+          <strong>Crawling and searching <a href="https://reddit.com/r/explainlikeimfive" target="_blank">reddit.com/r/explainlikeimfive</a></strong> by <a href="https://github.com/guillevc" target="_blank">Guillermo Varela</a> and <a href="https://github.com/luiscruzr8" target="_blank">Luis Cruz</a>.
         </p>
       </div>
     </footer>
